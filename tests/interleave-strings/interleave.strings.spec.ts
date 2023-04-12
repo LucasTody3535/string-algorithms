6cc9966c0b792c7c798cf82465f7b7d0aaf3a343
Toodie <lucas.vtodt@gmail.com>
2023-04-12 19:01:59 -0300

    add a test case for basic strings interleaving
    The test expects the inputs: 'abc' and '123' result in the ouput:
    'a1b2c3'

--- tests/interleave-strings/interleave.strings.spec.ts
+++ tests/interleave-strings/interleave.strings.spec.ts
@@ -7,4 +7,14 @@ describe("Interleave Strings Tests", () => {
     beforeEach(() => {
         stringInterleaver = new InterleaveStrings();
     });
+
+    it("Should interleave \"abc\" with \"123\" resulting in \"a1b2c3\"", () => {
+        const textOne = "abc";
+        const textTwo = "123";
+        const expected = "a1b2c3";
+
+        stringInterleaver.generateStringInterleavingTwoStrings(textOne, textTwo);
+
+        expect(stringInterleaver.getInterleavedStringWithCharactersAppended()).toBe(expected);
+    });
 });
