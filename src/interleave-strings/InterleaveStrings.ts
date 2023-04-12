e534958aa37bbfd5abc94e944ab5f55f2113a474
Toodie <lucas.vtodt@gmail.com>
2023-04-12 16:45:24 -0300

    add private method to get longer string in InterleaveStrings class
    The method is named 'getLongestStringFromASetOfStrings' and accepts an
    string varargs as argument, then it iterates over the given string list
    and updates local vars to keep track of the longest string. When it ends
    the iteration, it returns the longest string found. If the list has all
    elements with the same length, then "none" will be returned. It expects
    that the given array has at least two items, else will throw
    a RangeError.
    
    This method is necessary in interleave operations, where only strings
    with the same length are valid. But it is not suitable when the varargs
    contains strings with various lengths, an example would be:
    
    Input: 'ABCD', '12345', '!@#$%%¨'
    Output: '!@#$%%'
    
    In the given example, the third value is returned as the longest string
    but, there is another string which is '12345' that has an invalid
    length.

--- src/interleave-strings/InterleaveStrings.ts
+++ src/interleave-strings/InterleaveStrings.ts
@@ -8,6 +8,34 @@ export class InterleaveStrings {
         this.stringWithCharactersPopped = "";
         this.interleavedString = "";
     }
+
+    /**
+     * Returns the longest string from a set of strings or
+     * "none" if all strings are of same length
+     *
+     * @throws {RangeError} If the input array contains less than two strings
+     * @param {...Array<string>} strings - The array of strings to search
+     * @returns {string} The longest string from the input array or "none"
+     */
+    private getLongestStringFromASetOfStrings(...strings: Array<string>): string {
+        let longestString = "";
+        let longestStringLength = 0;
+
+        if(strings.length < 2) throw new RangeError("Expected string list to have at least two strings");
+
+        // Check if all the strings in the input array have the same length
+        if(!strings.every(value => value.length === strings.at(0)!.length)) return "none";
+
+        strings.forEach(value => {
+            if(strings.length > longestStringLength) {
+                longestString = value;
+                longestStringLength = value.length;
+            }
+        });
+
+        return longestString;
+    }
+
     /**
      * Pops characters from the end of the original string until its length matches the target length
      * @param originalString The original string
