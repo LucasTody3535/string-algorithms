25f41b116252224f7083179a8cb75248e048ec05
Toodie <lucas.vtodt@gmail.com>
2023-04-12 18:30:22 -0300

    add basic setup for a suite of tests related to InterleaveStrings class
    It define an global(inside of describe) var of type InterleaveStrings,
    then before each test that var receives a new instance of that class

---
+++ tests/interleave-strings/interleave.strings.spec.ts
@@ -0,0 +1,10 @@
+import { InterleaveStrings } from "../../src/interleave-strings/InterleaveStrings";
+
+describe("Interleave Strings Tests", () => {
+
+    let stringInterleaver!: InterleaveStrings;
+
+    beforeEach(() => {
+        stringInterleaver = new InterleaveStrings();
+    });
+});
