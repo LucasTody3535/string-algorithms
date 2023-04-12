b21a3700a30f61815fbfa246bb4c356837d17503
Toodie <lucas.vtodt@gmail.com>
2023-04-12 19:32:13 -0300

    add example of use of the algorithm with inputs and output in main file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-console.log(":D");
+import { InterleaveStrings } from "./interleave-strings/InterleaveStrings";
+
+const stringInterleaver = new InterleaveStrings();
+
+console.clear();
+console.log();
+
+console.log("------------- ALGORITHM TO INTERLEAVE STRINGS -------------");
+
+console.log();
+
+console.log(`Interleaving two strings
+Inputs: "abc", "123"
+Expected Output: a1b2c3`);
+stringInterleaver.generateStringInterleavingTwoStrings("abc", "123");
+console.log(`Actual Output:   ${stringInterleaver.getInterleavedStringWithCharactersAppended()}`);
+
+console.log("\n------------- Another Case -------------\n");
+
+console.log(`Interleaving two strings with different lengths
+Input: "abc", "1234"
+Expected Output: a1b2c34`);
+stringInterleaver.generateStringInterleavingTwoStrings("abc", "1234");
+console.log(`Actual Output:   ${stringInterleaver.getInterleavedStringWithCharactersAppended()}`);
+
+console.log();
