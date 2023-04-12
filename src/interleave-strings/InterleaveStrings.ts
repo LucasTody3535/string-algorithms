8fdc5f4ccdf65c6e5cb948982e8094237d6fc669
Toodie <lucas.vtodt@gmail.com>
2023-04-12 14:57:16 -0300

    add private prop in InterleaveStrings to store an interleaved string

--- src/interleave-strings/InterleaveStrings.ts
+++ src/interleave-strings/InterleaveStrings.ts
@@ -1,6 +1,7 @@
 export class InterleaveStrings {
     private charsPopped: string;
     private stringWithCharactersPopped: string;
+    private interleavedString: string;
 
     constructor() {
     }
