bed11db52e3b32fc76809aa8217159fbf0f2fadf
Toodie <lucas.vtodt@gmail.com>
2023-04-12 15:19:45 -0300

    add private method to InterleaveStrings class that pops chars
    The method is named 'popCharsUntilLengthMatches' and accepts two
    arguments: 'originalString' and 'targetLength'. The 'originalString'
    represents an string that will have its characters popped until its
    length reaches the value of 'targetLength'.
    
    The chars popped are stored in the private prop of the class named
    'charsPopped' for lates retrieval, the string resulting from popping
    operation will be stored in the private prop of the class named
    'stringWithCharactersPopped' for later retrieval too.

--- src/interleave-strings/InterleaveStrings.ts
+++ src/interleave-strings/InterleaveStrings.ts
@@ -8,5 +8,21 @@ export class InterleaveStrings {
         this.stringWithCharactersPopped = "";
         this.interleavedString = "";
     }
+    /**
+     * Pops characters from the end of the original string until its length matches the target length
+     * @param originalString The original string
+     * @param targetLength The target length
+     */
+    private popCharsUntilLengthMatches(originalString: string, targetLength: number) {
+        let charsPopped: Array<string> = [];
+        let originalStringSplitted = originalString.split("");
+
+        while(originalStringSplitted.length > targetLength) {
+            charsPopped.push(originalStringSplitted.pop()!);
+        }
+
+        this.charsPopped = charsPopped.join("");
+        this.stringWithCharactersPopped = originalStringSplitted.join("");
+    }
     }
 }
